eccf9bcba92a0fb17eca6d4c1ba2a6e2e94814cf
Felipe França <contatofelipefranca12@gmail.com>
2020-09-22 16:04:28 -0300

    Soma dois valores

--- Program.cs
+++ Program.cs
@@ -8,14 +8,18 @@ namespace Soma2Numeros
         {
             Console.Clear();
             Console.WriteLine("-----cálculo da soma entre dois números-----");
-            Console.Write("Digite o primeiro valor: "); //"valor1" digitado pelo usuário
-            decimal valor1 = Convert.ToDecimal(Console.ReadLine()); //conversão dos valores string para decimal
-            Console.Write("Digite o segundo valor: "); //"valor2" digitado pelo usuário
-            decimal valor2 = Convert.ToDecimal(Console.ReadLine()); //conversão dos valores string para decimal
-            decimal resultado = valor1 + valor2; //"resultado" fica armazenado a soma dos valores
-            Console.WriteLine($" {valor1} + {valor2} = {resultado}"); //exibe os valores digitados pelo usuário e o resultado da soma
-            Console.WriteLine("Pressione qualquer tecla para sair"); //Entrada de qualquer tecla digitada pelo usuário
-            Console.ReadKey(); //fim do programa
+
+            Console.Write("Digite o primeiro valor: ");
+            decimal valor1 = Convert.ToDecimal(Console.ReadLine()); 
+
+            Console.Write("Digite o segundo valor: "); 
+            decimal valor2 = Convert.ToDecimal(Console.ReadLine()); 
+
+            decimal resultado = valor1 + valor2; 
+            Console.WriteLine($" {valor1} + {valor2} = {resultado}");
+            
+            Console.WriteLine("Pressione qualquer tecla para sair"); 
+            Console.ReadKey(); 
         }
     }
 }
